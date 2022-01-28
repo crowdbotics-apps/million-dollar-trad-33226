db28948af5bfb0ca72dbdf13c0cc205bc122e212
Crowdbotics <team@crowdbotics.com>
2022-01-28 18:02:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "million_dollar_trad_33226",
+  url: "https://million_dollar_trad_33226.botics.co",
+  api: "https://million_dollar_trad_33226.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
